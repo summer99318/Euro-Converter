47a6f71386fd116c64165a84cfc3cf2103893054
summer99318 <69870582+summer99318@users.noreply.github.com>
2020-08-24 14:35:55 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main 
+{
+  public static void main(String[] args) 
+  
+  {
+    double dollar = 100;
+    double euro = 0.85;
+    double numEU;
+
+    numEU = dollar * euro;
+    
+    System.out.println("The number of Euros you have is " + numEU + "." + " The number of US dollars is " + numEU + ".");
+
+
+
+  }
+}
